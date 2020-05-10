4ae63aec4d59d726c33d3bb6918f4dee9cc0cce9
Jing Jie <jj@hireplace.com>
2020-05-10 12:01:41 +0800

    Updates

---
+++ precache-manifest.39c675d0462470686c0d94b67b81c370.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e52e7bc5fc58b1d3c764aed51a0e26b",
+    "url": "/winjingjie96/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b5746d9f38f52a1a59b5",
+    "url": "/winjingjie96/monsters-rolodex/static/css/main.a9f0237e.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/winjingjie96/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/winjingjie96/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5746d9f38f52a1a59b5",
+    "url": "/winjingjie96/monsters-rolodex/static/js/main.450f4e87.chunk.js"
+  },
+  {
+    "revision": "98976f819e3707fe002f",
+    "url": "/winjingjie96/monsters-rolodex/static/js/runtime-main.3b0a7f53.js"
+  }
+]);
